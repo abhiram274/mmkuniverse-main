b08a64a174cf16d096065cc3c948211d779f0f74
abhiram274 <abhiramkosuru967@gmail.com>
2025-06-05 21:42:25 +0530

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -43,7 +43,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://mmkuniverse-main.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
