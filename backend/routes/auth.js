475460451e5224beec1c3a17e646d89d998f5671
VARA-06 <varakosuru@gmail.com>
2025-06-14 23:01:46 +0530

    ok done sessions

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,4 +1,4 @@
-// const express = require('express');
+const express = require('express');
 const bcrypt = require('bcrypt');
 const db = require('../db');
 const sendOTPviaEmail = require('../utils/email'); // Adjust path
