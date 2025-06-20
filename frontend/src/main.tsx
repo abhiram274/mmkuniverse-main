ebf736c75879261e435ab889266b1d2c479def17
Abhiram Kosuru <abhiramkosuru967@gmail.com>
2025-06-20 15:31:34 +0530

    modified:   package-lock.json         modified:   package.json         modified:   src/main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,21 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-createRoot(document.getElementById("root")!).render(<App />);
-
 // import { createRoot } from 'react-dom/client'
 // import App from './App.tsx'
 // import './index.css'
-// import { GoogleOAuthProvider } from '@react-oauth/google';
 
-// createRoot(document.getElementById("root")!).render(
-//   <GoogleOAuthProvider clientId="715736192011-07ol75qt1qse82hqtq8rhsije9qf8pjk.apps.googleusercontent.com">
-//     <App />
-//   </GoogleOAuthProvider>
-// );
+// createRoot(document.getElementById("root")!).render(<App />);
+
+
+//715736192011-07ol75qt1qse82hqtq8rhsije9qf8pjk.apps.googleusercontent.com
+
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const clientId = "715736192011-07ol75qt1qse82hqtq8rhsije9qf8pjk.apps.googleusercontent.com"; // Replace with actual client ID
+
+createRoot(document.getElementById("root")!).render(
+  <GoogleOAuthProvider clientId={clientId}>
+    <App />
+  </GoogleOAuthProvider>
+);
