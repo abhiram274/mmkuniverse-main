724bac71f219ebba429674a8f773860530cc3bc2
Abhiram Kosuru <abhiramkosuru967@gmail.com>
2025-06-20 13:37:49 +0530

    main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,22 @@
-// import { createRoot } from 'react-dom/client'
-// import App from './App.tsx'
-// import './index.css'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-// createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<App />);
 
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const CLIENT_ID = '715736192011-07ol75qt1qse82hqtq8rhsije9qf8pjk.apps.googleusercontent.com';
+// import React from 'react'; 
+// import { createRoot } from 'react-dom/client';
+// import App from './App.tsx';
+// import './index.css';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
 
-createRoot(document.getElementById('root')!).render( 
-  <GoogleOAuthProvider clientId={CLIENT_ID}>
-    <App />
-  </GoogleOAuthProvider>
-);
+// const CLIENT_ID = '715736192011-07ol75qt1qse82hqtq8rhsije9qf8pjk.apps.googleusercontent.com';
+
+// createRoot(document.getElementById('root')!).render( 
+//      <React.StrictMode>
+//   <GoogleOAuthProvider clientId={CLIENT_ID}>
+//     <App />
+//   </GoogleOAuthProvider>
+//   </React.StrictMode>
+// );
