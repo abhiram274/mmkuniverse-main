0bb83f671f47d37fc059985aab0065ee7014def0
Abhiram Kosuru <abhiramkosuru967@gmail.com>
2025-06-20 12:50:10 +0530

    messages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,7 +11,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const CLIENT_ID = '715736192011-07ol75qt1qse82hqtq8rhsije9qf8pjk.apps.googleusercontent.com';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render( 
   <GoogleOAuthProvider clientId={CLIENT_ID}>
     <App />
   </GoogleOAuthProvider>
